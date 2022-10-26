a0c45519174befc630c71badf2a9150a70b55962
m/s <m@umbr-ch.de>
2022-10-26 08:41:43 +0200

    updated text about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,20 +6,32 @@ export default function about() {
     <Layout>
       <div>
         <h1>About</h1>
+        <h3>Working together</h3>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
-          earum! Recusandae iste repellendus in sapiente ab aperiam, architecto
-          quae obcaecati perferendis odio.
+          In order to form innovative, effective and individual design
+          solutions, we believes in a close dialogue with our clients and in a
+          profound understanding of their objectives. The studio is a creative
+          partner for individuals, commercial companies, like-minded businesses
+          and cultural or academical institutions.
         </p>
+        <h3>Working fields</h3>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
-          earum! Recusandae iste repellendus in sapiente ab aperiam, architecto
-          quae obcaecati perferendis odio.
+          Visual identities, corporate designs & branding / books, publications
+          & magazines / webdesign & digital applications / exhibition graphics &
+          signage
         </p>
+        <h3>Selected clients</h3>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
-          earum! Recusandae iste repellendus in sapiente ab aperiam, architecto
-          quae obcaecati perferendis odio.
+          Bündner Kunstmuseum, Chur Cabaret Voltaire, Zürich Filmcoopi, Zürich
+          Museum zu Allerheiligen, Schaffhausen Bruno Augsburger Photography,
+          Zürich Fachhochschule Nordwestschweiz HKB Hochschule der Künste Bern
+          Stiftung Netzwerk Waldrap Architekten Zuriga Coffee
+        </p>
+        <h3>Network</h3>
+        <p>
+          For interdisciplinary projects and wider challenges the studio
+          collaborates with fellow specialists from the fields of programming,
+          photography, illustration, animation, text and type design.
         </p>
       </div>
     </Layout>
